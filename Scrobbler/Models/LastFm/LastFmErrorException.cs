8dabd70c4c0e5c03738416a2b018d120f6bd01d1
Riven Skaye <riven@tae.moe>
2025-04-16 09:40:46 +0200

    Oops - `Code` was not hiding base members

--- Scrobbler/Models/LastFm/LastFmErrorException.cs
+++ Scrobbler/Models/LastFm/LastFmErrorException.cs
@@ -1,3 +1,6 @@
 ﻿namespace Scrobbler.Models.LastFm;
 
-public class LastFmErrorException(int code, string message) : ApplicationException($"LastFm error: {code} - {message}");
+public class LastFmErrorException(int code, string message) : ApplicationException($"LastFm error: {code} - {message}")
+{
+    public int Code { get; } = code;
+}
