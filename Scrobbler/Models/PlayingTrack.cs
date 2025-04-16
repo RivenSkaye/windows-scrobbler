033c962be132e000e1e9ce4d8832c1a0910f0bf4
Riven Skaye <riven@tae.moe>
2025-04-16 08:41:48 +0200

    Use C# 11 (dotnet 7+) `required` and C# 9 `init` for non-nullable props

--- Scrobbler/Models/PlayingTrack.cs
+++ Scrobbler/Models/PlayingTrack.cs
@@ -16,38 +16,38 @@ public record TrackMetadata()
     /// <summary>
     /// The name of the track
     /// </summary>
-    public string TrackName { get; }
-    
+    public required string TrackName { get; init; }
+
     /// <summary>
     /// The duration of the track
     /// </summary>
     public TimeSpan TrackDuration { get; set; }
-    
+
     /// <summary>
     /// The name of the track's artist
     /// </summary>
-    public string ArtistName { get; }
-    
+    public required string ArtistName { get; init; }
+
     /// <summary>
     /// The name of the track's album
     /// </summary>
     public string? AlbumName { get; }
-    
+
     /// <summary>
     /// The name of the track's album's artist. Can be null if the same as the track artist name
     /// </summary>
     public string? AlbumArtistName { get; }
-    
+
     /// <summary>
     /// the track number of the track
     /// </summary>
     public int? TrackNumber { get; }
-    
+
     /// <summary>
     /// The time the track started playing, in UTC
     /// </summary>
     public DateTime PlayingSince { get; }
-    
+
     public bool IsQueuedForScrobble { get; set; }
 
     public bool IsSameTrackAs(TrackMetadata? other) => other is not null
